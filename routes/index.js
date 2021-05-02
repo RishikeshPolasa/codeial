624e7d4646185d0080e0636c3ece7d3589013f0a
Rishikesh Polasa <rishipolasa@gmail.com>
2021-05-02 08:05:17 +0530

    add controller for user and post

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,11 @@ const homeController = require("../controllers/home_controller");
 console.log("express router is running");
 
 router.get("/", homeController.home);
+router.use("/users", require("./users"));
+
+//for any further routes,acces from here
+//router.use('/routerName',require('./routerFile'))
+
+router.use("/users", require("./posts"));
 
 module.exports = router;
