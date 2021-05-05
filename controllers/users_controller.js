1052b9ecd820c9abefee084a61db87781f447f69
RishikeshPolasa <rishipolasa@gmail.com>
2021-05-05 08:57:10 +0530

    add signup route to the app

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,52 @@ module.exports.profile = function (req, res) {
     title: "user",
   });
 };
+
+const User = require("../models/user");
+
+//render the sign up
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Codeial | sign Up",
+  });
+};
+
+//render the sing in
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Codeial | sign In",
+  });
+};
+
+//get the signUp data
+
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding user in signing up");
+      return;
+    }
+
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("error in creating user in while signing up");
+          return;
+        }
+
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
+
+//sign in and create session for the user
+
+module.exports.createSession = function (req, res) {
+  //todo later
+};
